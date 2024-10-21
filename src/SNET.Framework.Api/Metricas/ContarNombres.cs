9bfe7e3e6b4c8d37eb5ea510dcf257fb8c5aa14b
SIEMPRE.NET S.AS. <siempre.net@hotmail.com>
2024-10-21 17:41:29 -0500

    Añadir Docker Compose y OpenTelemetry
    Se ha añadido un nuevo proyecto `docker-compose` al archivo de solución `SNETFRAMEWORK.sln` para habilitar la configuración de Docker Compose. Se han agregado configuraciones de compilación y ejecución para este proyecto en la sección `Global` del archivo de solución.
    
    En `Program.cs`, se ha añadido la configuración para OpenTelemetry, incluyendo métricas, trazas y registros. También se ha comentado la línea `builder.AddLogger()`. La ruta raíz del endpoint se ha modificado para aceptar un parámetro `name` y registrar el nombre utilizando OpenTelemetry.
    
    En el archivo de proyecto `SNET.Framework.Api.csproj`, se han añadido propiedades relacionadas con Docker y nuevas referencias de paquetes para OpenTelemetry y herramientas de contenedores de Azure.
    
    Se han añadido varios archivos para la configuración de Docker:
    - `.dockerignore` para excluir ciertos archivos y directorios del contexto de compilación.
    - `docker-compose.dcproj` para definir la configuración del proyecto Docker Compose.
    - `docker-compose.override.yml` para configuraciones específicas del entorno de desarrollo.
    - `docker-compose.yml` para definir los servicios y la configuración de construcción.
    - `launchSettings.json` para configurar el perfil de inicio en Visual Studio.
    - `Dockerfile` para definir las fases de construcción y ejecución del contenedor.
    
    Finalmente, se ha creado una nueva clase `ContarNombres.cs` para definir una métrica de contador utilizando OpenTelemetry.

---
+++ src/SNET.Framework.Api/Metricas/ContarNombres.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace SNET.Framework.Api.Metricas;
+
+public static class ContarNombres
+{
+    public  const string ServiceName = "ContarNombres";
+
+    public static Meter Metrica = new Meter(ServiceName);
+
+    public static Counter<int> Contar = Metrica.CreateCounter<int>("TotalNombre");
+}
--- src/SNET.Framework.Api/Program.cs
+++ src/SNET.Framework.Api/Program.cs
@@ -1,7 +1,12 @@
 using Carter;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using SNET.Framework.Api.DependencyConfig;
+using SNET.Framework.Api.Metricas;
 using SNET.Framework.Domain.Notifications.Email;
 using SNET.Framework.Infrastructure.Notifications.Email;
 using SNET.Framework.Persistence;
@@ -11,8 +16,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
 
 builder.AddRepositories();
-builder.AddLogger();
+//builder.AddLogger();
 builder.AddEmailSettings();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resuorse =>
+    {
+        resuorse.AddService("FrameworkApi");
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+
+        metrics.AddOtlpExporter();
+    })
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation();
+
+        tracing.AddOtlpExporter();
+    });
+
+    builder.Logging.AddOpenTelemetry(loggin => loggin.AddOtlpExporter());
 
 builder.Services.AddMediatR(x=>
 {
@@ -47,6 +77,11 @@ app.UseReDoc(settings =>
 
 app.MapCarter();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/{name}", (ILogger<Program> logger, string name) =>
+{
+    ContarNombres.Contar.Add(1, new KeyValuePair<string, object>("nombre", name));
+    logger.LogInformation("Su nombre es {name}", name);
+    return name;
+});
 
 app.Run();
