c2ae893cec552f729b3a0fad51b60e37bf21fa41
MasterMindAlex01 <30800345+MasterMindAlex01@users.noreply.github.com>
2024-10-30 20:36:57 -0500

    Pruebas unitarias

---
+++ tests/SNET.Framework.IntegrationTesting/EndPoints/UserEndPointsTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using SNET.Framework.Domain.Shared;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using SNET.Framework.Features.Users.Commands;
+using System.Text.Json;
+using System.Net.Http.Json;
+
+namespace SNET.Framework.IntegrationTesting.EndPoints;
+
+public class UserEndPointsTests : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private const string urlController = "/Users";
+    private readonly HttpClient _client;
+    private string token = string.Empty;
+
+    public UserEndPointsTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CreateUser_ReturnsOkResponse()
+    {
+        // Arrange
+        var newSupplier = new CreateUserCommand()
+        {
+            Email = "test@example.com",
+            FirstName = "Test",
+            Id = Guid.NewGuid(),
+            LastName = "Test",
+            Password = "123456",
+            PhoneNumber = "3135030716",
+        };
+        var content = new StringContent(JsonSerializer.Serialize(newSupplier), Encoding.UTF8, "application/json");
+
+
+        // Act
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await _client.PostAsync($"{urlController}", content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var createdId = await response.Content.ReadFromJsonAsync<Result>();
+        var result = Result.Success("Usuario creado correctamente");
+
+        Assert.NotEqual(result, createdId);
+    }
+
+}
---
+++ tests/SNET.Framework.Tests/Users/Commands/CreateUser/CreateUserCommandHandlerTests.cs
@@ -0,0 +1,102 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+using SNET.Framework.Domain.Entities;
+using SNET.Framework.Domain.Repositories;
+using SNET.Framework.Domain.Shared;
+using SNET.Framework.Domain.UnitOfWork;
+using SNET.Framework.Features.Users.Commands;
+
+namespace SNET.Framework.Tests.Users.Commands.CreateUser;
+
+public class CreateUserCommandHandlerTests
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IValidator<CreateUserCommand>> _validatorMock;
+    private readonly CreateUserCommandHandler _handler;
+
+    public CreateUserCommandHandlerTests()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _validatorMock = new Mock<IValidator<CreateUserCommand>>();
+
+        _handler = new CreateUserCommandHandler(
+            _userRepositoryMock.Object,
+            _unitOfWorkMock.Object,
+            _validatorMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ValidCommand_ShouldAddUserAndCommit()
+    {
+        // Arrange
+        var resultResponse = Result.Success("Usuario creado correctamente");
+        var validResult = new ValidationResult();
+        
+        var command = new CreateUserCommand()
+        {
+            Id = Guid.NewGuid(),
+            Email = "alex@example.com",
+            FirstName = "Test",
+            LastName = "Test",
+            Password = "123456",
+            PhoneNumber = "3135241205"
+        };
+
+        _validatorMock
+            .Setup(x => x.Validate(It.IsAny<CreateUserCommand>()))
+            .Returns(validResult);
+
+        // Act
+        Result result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        _userRepositoryMock
+            .Verify(repo => repo.Add(It.IsAny<User>()), Times.Once);
+        _unitOfWorkMock
+            .Verify(uow => uow.SaveChangesAsync(CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_InvalidCommand_ShouldValidation()
+    {
+        // Arrange
+        var command = new CreateUserCommand { Email = "" };
+        var validationFailures = new List<ValidationFailure>
+        {
+            new ValidationFailure("Email", "Email cannot be empty")
+        };
+        _validatorMock.Setup(v => v.Validate(command)).Returns(new ValidationResult(validationFailures));
+
+        // Act
+        Result result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(result.Error);
+        Assert.Equal("Datos no válidos", result.Error!.Message);
+    }
+
+    [Fact]
+    public async Task Handle_ValidCommand_ShouldNotCallCommitIfAddFails()
+    {
+        // Arrange
+        var command = new CreateUserCommand
+        {
+            Id = Guid.NewGuid(),
+            Email = "alex@example.com",
+            FirstName = "Test",
+            LastName = "Test",
+            Password = "123456",
+            PhoneNumber = "3135241205"
+        };
+        _validatorMock.Setup(v => v.Validate(command)).Returns(new ValidationResult());
+        _userRepositoryMock.Setup(repo => repo.Add(It.IsAny<User>())).Throws(new Exception("Add failed"));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<Exception>(() => _handler.Handle(command, CancellationToken.None));
+        _unitOfWorkMock.Verify(uow => uow.SaveChangesAsync(CancellationToken.None), Times.Never);
+    }
+}
